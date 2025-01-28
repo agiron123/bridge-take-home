ca14ed94e0ee3dd8585f7bfc626a506bacc14c8e
Andre Giron <agiron123@gmail.com>
2025-01-28 00:50:43 -0800

    Implements the details view and refactors code

--- app/pokemon/[id]/page.tsx
+++ app/pokemon/[id]/page.tsx
@@ -1,34 +1,113 @@
-import { Box, Flex, Image, Text } from '@mantine/core';
+'use client';
 
-const data = {
-  name: 'Pikachu',
-  sprites: {
-    front_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-  },
-  height: 4,
-  weight: 60,
-  abilities: [{ ability: { name: 'Static' } }],
-  types: [{ type: { name: 'Electric' } }],
-};
+import { Accordion, Box, Container, Flex, ScrollArea, Text, Title } from '@mantine/core';
+import { getPokemonDetails } from '@/route-handlers/pokemon';
+import { useEffect, useState } from 'react';
+import { LoadingOverlay } from '@mantine/core';
+import { usePathname } from 'next/navigation';
+import { Carousel } from '@mantine/carousel';
+import getCarouselItems from '@/components/Carouseltems/getCarouselItems';
+import Link from 'next/link';
+import { PokemonDetails, Sprite } from '@/components/DetailLists/types';
+import { AbilitiesList, StatsList, TypesList } from '@/components/DetailLists/DetailLists';
+import { MovesList } from '@/components/DetailLists/DetailLists';
 
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-  const id = (await params).id;
+export default function Page() {
+  const pathname = usePathname();
+  const id = pathname.split('/')[2];
 
-  console.log('id', id);
-  console.log('params', params);
+  const [data, setData] = useState<PokemonDetails | null>(null);
+  const [isPending, setIsPending] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState<unknown | null>(null);
+
+  useEffect(() => {
+    async function fetchPokemonDetails() {
+      try {
+        setIsPending(true);
+        const data = await getPokemonDetails(id);
+        setData(data);
+      } catch (error) {
+        setIsError(true);
+        setError(error);
+      } finally {
+        setIsPending(false);
+      }
+    }
+    fetchPokemonDetails();
+  }, [id]);
+
+  if (isError && error) {
+    return <div>Error fetching pokemon details</div>;
+  }
+
+  if (isPending && !data) {
+    return (
+      <Box w="100%" h="100vh" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+        <LoadingOverlay visible={isPending} />
+      </Box>
+    );
+  }
+
+  const carouselItems = getCarouselItems(data?.sprites as Sprite, data?.name as string);
 
   return (
-    <Box>
-      <Text>{data.name.charAt(0).toUpperCase() + data.name.slice(1)}</Text>
-      <Image src={data.sprites.front_default} alt={data.name} />
-      <Flex direction="column">
-        <Text>Height: {data.height}</Text>
-        <Text>Weight: {data.weight}</Text>
-        <Text>
-          Abilities: {data.abilities.map((ability: any) => ability.ability.name).join(', ')}
-        </Text>
-        <Text>Types: {data.types.map((type: any) => type.type.name).join(', ')}</Text>
+    <Container m="auto" w="100%" h="100vh" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <Flex justify="flex-start" w="100%">
+        <Link href="/">
+          Home
+        </Link>
       </Flex>
-    </Box>
+      <Title order={1}>{data?.name ? data.name.charAt(0).toUpperCase() + data.name.slice(1) : ''}</Title>
+      {
+        carouselItems && carouselItems.length > 0 && (
+          <Carousel
+            withIndicators
+            height={200}
+            slideSize="33.333333%"
+            slideGap="md"
+            loop
+            align="start"
+            slidesToScroll={3}
+          >
+            {carouselItems}
+          </Carousel>
+        )
+      }
+
+      <Flex direction="column" gap="md">
+        <StatsList stats={data?.stats || []} />
+        <Text><strong>Height:</strong> {data?.height}</Text>
+        <Text><strong>Weight:</strong> {data?.weight}</Text>
+        <Accordion variant="contained" w="250px">
+          <Accordion.Item value="types">
+            <Accordion.Control><strong>Types</strong></Accordion.Control>
+            <Accordion.Panel>
+              <ScrollArea h={200} w="100%">
+                <TypesList types={data?.types || []} />
+              </ScrollArea>
+            </Accordion.Panel>
+          </Accordion.Item>
+
+          <Accordion.Item value="moves">
+            <Accordion.Control><strong>Moves</strong></Accordion.Control>
+            <Accordion.Panel>
+              <ScrollArea h={200} w="100%">
+                <MovesList moves={data?.moves || []} />
+              </ScrollArea>
+            </Accordion.Panel>
+          </Accordion.Item>
+
+          <Accordion.Item value="abilities">
+            <Accordion.Control><strong>Abilities</strong></Accordion.Control>
+            <Accordion.Panel>
+              <ScrollArea h={200} w="100%">
+                <AbilitiesList abilities={data?.abilities || []} />
+              </ScrollArea>
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+      </Flex>
+    </Container >
   );
 }
